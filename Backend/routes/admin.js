7cffaf38e0c5b3bfada8ae3e08f49e8085c0cdb5
yash kumar <yashkumar.967565@gmail.com>
2025-12-22 15:03:46 +0530

    Added admin routes

---
+++ Backend/routes/admin.js
@@ -0,0 +1,37 @@
+const {Router} = require("express");
+const adminRouter = Router();
+
+
+adminRouter.post("/signup",function(req,res){
+    res.json({
+        message: "signup endpoint"
+    })
+})
+
+adminRouter.post("/signin",function(req,res){
+    res.json({
+        message: "signin endpoint"
+    })
+})
+
+adminRouter.post("/course",function(req,res){
+    res.json({
+        message: "course endpoint"
+    })
+})
+
+adminRouter.put("/course",function(req,res){
+    res.json({
+        message: "Change the Course"
+    })
+})
+
+adminRouter.get("/course/bulk",function(req,res){
+    res.json({
+        message: "All the course endpoint"
+    })
+})
+
+module.exports = {
+    adminRouter : adminRouter
+}
