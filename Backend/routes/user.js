0e7789778c128b42e4316c3845c22b0f79126161
yash kumar <yashkumar.967565@gmail.com>
2025-12-23 10:22:37 +0530

    signup signin logic user routes

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,14 +1,43 @@
-const {Router} = require("express")
-
+const {Router} = require("express");
+const {userModel} = require("../db");
+const jwt = require("jsonwebtoken")
+const JWT_USER_PASSWORD = "aladid123"
 const userRouter = Router();
 
-    userRouter.post("/signup",function(req,res){
+    userRouter.post("/signup", async function(req,res){
+       const {email,password,firstname,lastname} = req.body;
+       await userModel.create({
+            email: email,
+            password: password,
+            firstname: firstname,
+            lastname: lastname
+        })
     res.json({
-        message: "signup endpoint"
+        message: "Signup Succeeded "
     })
 })
 
-userRouter.post("/signin",function(req,res){
+userRouter.post("/signin",async function(req,res){
+    const {email,password} = req.body;
+
+    const user = await userModel.findOne({
+        email: email,
+        password: password
+    })
+
+    if(user){
+    const token =  jwt.sign({
+        id: user._id
+     },JWT_USER_PASSWORD);
+     res.json({
+        token: token
+    
+     })
+    }else{
+        res.status(403).json({
+            message: "Incorrect credentials"
+        })
+    }
     res.json({
         message: "signin endpoint"
     })
